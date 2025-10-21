24fb3dfcf03f714c8e5b84a56d6f3f29cd86d437
Wrench0-0 <202380356@psu.palawan.edu.ph>
2025-10-21 21:49:14 +0800

    updated part 6

--- project/taskorg/views.py
+++ project/taskorg/views.py
@@ -1,36 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from taskorg.models import Organization
+from taskorg.models import Program
 from taskorg.forms import OrganizationForm 
 from django.urls import reverse_lazy
 from django.db.models import Q
 from django.utils import timezone
 
-class HomePageView(ListView):
-    model = Organization
-    context_object_name = 'home'
-    template_name = "home.html"
-    
-    def get_context_data(self, **kwargs):
-       context = super().get_context_data(**kwargs)
-       context["total_students"] = Student.objects.count()
-       
-       today = timezone.now().date()
-       count = (
-          OrgMember.objects.filter(
-             date_joined__year=today.year
-             )
-             .values("student")
-             .distinct()
-             .count()
-        )
-       
-       context["students_joined_this_year"] = count
-       return context
+class ProgramList(ListView):
+   model = Program
+   context_object_name = 'program'
+   template_name = 'program_list.html'
+   paginate_by = 5
+
+   def get_ordering(self):
+      allowed = ["prog_name", "college__college_name"]
+      sort_by = self.request.GET.get("sort_by")
+      if sort_by in allowed:
+         return sort_by
+      return "prog_name"
 
 class OrganizationList(ListView):
- model = Organization
+ model = Program
  context_object_name = 'organization'
  template_name = 'org_list.html'
  paginate_by = 5
@@ -48,19 +39,19 @@ class OrganizationList(ListView):
        return qs
 
  class OrganizationCreateView(CreateView):
-    model = Organization
+    model = Program
     form_class = OrganizationForm
     template_name = 'org_form.html'
     success_url = reverse_lazy('organization-list')
 
  class OrganizationUpdateView(UpdateView):
-    model = Organization
+    model = Program
     form_class = OrganizationForm
     template_name = 'org_form.html'
     success_url = reverse_lazy('organization-list')
 
  class OrganizationDeleteView(DeleteView):
-    model = Organization
+    model = Program
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
 # Create your views here.
